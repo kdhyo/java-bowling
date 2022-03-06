bb3600295353bf5c6e8e7a97e5016c3d4044e5ee
dongHyo <kdhyo98@gmail.com>
2022-03-06 15:08:27 +0900

    test : NormalFrame 테스트 케이스 작성

--- src/test/java/bowling/domain/frame/NormalFrameTest.java
+++ src/test/java/bowling/domain/frame/NormalFrameTest.java
@@ -2,6 +2,8 @@ package bowling.domain.frame;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import bowling.domain.Pins;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class NormalFrameTest {
@@ -9,6 +11,21 @@ class NormalFrameTest {
 	@Test
 	void 게임시작() {
 		assertThat(NormalFrame.start()).isInstanceOf(NormalFrame.class);
+	}
+
+	@Test
+	@DisplayName("NormalFrame 스트라이크 9번 시 마지막 FinalFrame 객체 체크")
+	void bowling() {
+		// given
+		Frame frame = NormalFrame.start();
+
+		// when
+		for (int i = 0; i < 9; i++) {
+			frame = frame.bowling(Pins.hit(10));
+		}
+
+		// then
+		assertThat(frame).isInstanceOf(FinalFrame.class);
 	}
 
 }
