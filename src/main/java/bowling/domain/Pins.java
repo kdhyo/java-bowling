ca47f460e304e264b7bce04f0aac27fedbeb2bbb
dongHyo <kdhyo98@gmail.com>
2022-03-06 13:47:02 +0900

    feat : Pins 클래스 구현

---
+++ src/main/java/bowling/domain/Pins.java
@@ -0,0 +1,35 @@
+package bowling.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Pins {
+
+	private static final int MINIMUM_PIN_SIZE = 0;
+	private static final int MAXIMUM_PIN_SIZE = 10;
+
+	private static final List<Pins> PINS;
+
+	private final int pins;
+
+	static {
+		PINS = IntStream.rangeClosed(MINIMUM_PIN_SIZE, MAXIMUM_PIN_SIZE)
+			.mapToObj(Pins::new)
+			.collect(Collectors.toCollection(ArrayList::new));
+	}
+
+	public static Pins of(int pins) {
+		try {
+			return PINS.get(pins);
+		} catch (IndexOutOfBoundsException e) {
+			throw new IllegalArgumentException(String.format("핀의 수는 %d ~ %d 사이만 가능합니다.", MINIMUM_PIN_SIZE, MAXIMUM_PIN_SIZE));
+		}
+	}
+
+	private Pins(int pins) {
+		this.pins = pins;
+	}
+
+}
