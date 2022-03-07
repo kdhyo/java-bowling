b1f36fd9eaad72127f08b5f1b14004b2b8e26bf6
dongHyo <kdhyo98@gmail.com>
2022-03-07 23:17:33 +0900

    refactor : score message add

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -16,6 +16,7 @@ public class InputView {
 
 	public static int score(int round) {
 		try {
+			System.out.print(String.format("%d프레임 투구 : ", round));
 			return Integer.parseInt(SCANNER.nextLine());
 		} catch (NumberFormatException e) {
 			throw new NumberFormatException("숫자만 입력할 수 있습니다.");
