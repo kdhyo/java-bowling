d204585d0897a6e2cc6418d290ca74b44dd9ce27
dongHyo <kdhyo98@gmail.com>
2022-03-07 23:27:11 +0900

    refactor : 점수판 여백 수정

--- src/main/java/bowling/view/ScoreBoard.java
+++ src/main/java/bowling/view/ScoreBoard.java
@@ -1,15 +1,14 @@
 package bowling.view;
 
-import bowling.domain.Player;
 import bowling.domain.frame.Frame;
-import bowling.domain.frame.Frames;
 import java.util.List;
 
 public class ScoreBoard {
 
-	private static final String ROUND_BOARD = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |";
-	private static final String FORMAT = "|  %-3s |";
-	private static final String DEFAULT_FORMAT = "|      |";
+	private static final String ROUND_BOARD = "|  NAME  |   01   |   02   |   03   |   04   |   05   |   06   |   07   |   08   |   09   |   10   |";
+	private static final String NAME_FORMAT = "|   %-3s  |";
+	private static final String FRAME_FORMAT = "  %-5s |";
+	private static final String DEFAULT_FORMAT = "        |";
 	private static final int LAST_ROUND = 10;
 
 	private ScoreBoard() {
@@ -22,7 +21,7 @@ public class ScoreBoard {
 		int currentRound = frames.size();
 
 		for (Frame frame : frames) {
-			scoreBoard.append(String.format(FORMAT, frame.frameScore()));
+			scoreBoard.append(String.format(FRAME_FORMAT, frame.frameScore()));
 		}
 
 		for (int i = currentRound; i < LAST_ROUND; i++) {
@@ -33,7 +32,7 @@ public class ScoreBoard {
 	}
 
 	private static StringBuilder scoreBoardStart(String name) {
-		return new StringBuilder(String.format(FORMAT, name));
+		return new StringBuilder(String.format(NAME_FORMAT, name));
 	}
 
 }
